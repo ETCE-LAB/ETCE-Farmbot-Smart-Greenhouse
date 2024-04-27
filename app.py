30863a11c0a4bfd77ae7d30c4357fc18a9318683
tux <ma.knigge@ostfalia.de>
2024-04-27 11:23:45 +0200

    Rewrote the whole file. Seems to work but DNS error...

--- app.py
+++ app.py
@@ -1,49 +1,51 @@
-from flask import Flask
-from apscheduler.schedulers.background import BackgroundScheduler
 import csv
 import time
 import ttn
+from flask import Flask
+from apscheduler.schedulers.background import BackgroundScheduler
 
 app = Flask(__name__)
-
-# Initialize CSV file
 csv_file_path = 'ttn_data.csv'
-with open(csv_file_path, mode='w', newline='') as file:
-    writer = csv.writer(file)
-    writer.writerow(['Device ID', 'Payload', 'Timestamp'])  # Adjust headers based on actual data structure
+
+app_id = "eui-70b3d57ed005ea4a"
+access_key = "NNSXS.DWWKJ3GCY4SX6SYXOFIHEA2SUMFRC6UQSE7BFFI.YAJWGXZJTYZAXKPE7NJBF6MDPMGH5CNYAZ4XHHBUIETCGFLA4EEA"
+
+
+def uplink_callback(msg, client):
+    print("Received uplink from ", msg.dev_id)
+    print(msg)
+    # Write to CSV
+    with open(csv_file_path, mode='a', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow([msg.dev_id, msg.payload_raw, msg.metadata.time])
+
+
+# Setup TTN MQTT Client
+handler = ttn.HandlerClient(app_id, access_key)
+mqtt_client = handler.data()
 
 
 def scheduled_task():
-    app_id = "tuc-isse-sensorik"
-    access_key = "NNSXS.BGN4W6LCYDLSGVATWTPPMU7URARU6NO3HCCA4BI.KBHIS4VN65K32UK57QLHP2NA6PXULQ4Y2IWXGHUAN5VIBOZUPQUQ"  # TODO: Hide API key!
-
-    def uplink_callback(msg, client):
-        print("Received uplink from ", msg.dev_id)
-        print(msg)
-        # Write to CSV
-        with open(csv_file_path, mode='a', newline='') as file:
-            writer = csv.writer(file)
-            writer.writerow([msg.dev_id, msg.payload_raw, msg.metadata.time])
-
-    handler = ttn.HandlerClient(app_id, access_key)
-
-    mqtt_client = handler.data()
     mqtt_client.set_uplink_callback(uplink_callback)
     mqtt_client.connect()
-    time.sleep(60)
+    time.sleep(60)  # Keep the connection open for 60 seconds
     mqtt_client.close()
+    print("MQTT client ran for 60 seconds.")
 
-    print("Executing every 15 minutes.")
 
+# Initialize CSV file with headers
+with open(csv_file_path, mode='w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(['Device ID', 'Payload', 'Timestamp'])
 
 scheduler = BackgroundScheduler()
-scheduler.add_job(scheduled_task, 'interval', minutes=15)
+scheduler.add_job(scheduled_task, 'interval', minutes=15)  # Runs every 15 minutes
 scheduler.start()
 
 
 @app.route('/')
 def home():
-    return "Scheduler is running!"
+    return "Scheduler is running and listening to TTN!"
 
 
 if __name__ == '__main__':
