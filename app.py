d0a019548e150bc9a81a76856f6d80a8907a9735
Mattes <m.knigge@ostfalia.de>
2024-04-29 16:18:39 +0200

    Implemented Flask and created two endpoints. One for fetching data and one for accessing the stored data as JSON. API key is now hidden inside config.py and this file will not be published because of the .gitignore

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
+from flask import Flask, jsonify
 import json
 import requests
 import csv
 from apscheduler.schedulers.background import BackgroundScheduler
 import datetime
-import time
+import config
+
+app = Flask(__name__)
 
 
 def fetch_and_process_data():
     print(f"Fetching data at {datetime.datetime.now().strftime('%m-%d %H:%M')}")
-    response = requests.get(device_url, headers=headers)
+    response = requests.get(config.device_url, headers=headers)
     if response.status_code == 200:
         print("Data retrieval successful, processing data...")
         handle_partial_json(response.text)
@@ -19,47 +22,70 @@ def fetch_and_process_data():
 def handle_partial_json(text):
     try:
         index = text.rfind('{"result"')
-        print(text[index:])
+        #  print(text[index:])
         data = json.loads(text[index:])
         messages = data['result']['uplink_message']['decoded_payload']['messages']
         received_at = data['result']['received_at']
 
         with open('weather_data.csv', 'a', newline='') as file:
             writer = csv.writer(file)
-
             for message in messages:
                 writer.writerow([message['measurementValue'], message['type'], received_at])
 
-        print("Partial weather data written to CSV successfully.")
+        print("Weather data written to CSV successfully.")
+        print("Awaiting next data fetch...")
     except Exception as e:
         print("Failed to parse truncated JSON:", str(e))
 
 
-application_id = 'tuc-isse-sensorik'
-device_id = 'eui-70b3d57ed005ea4a'
-access_key = 'NNSXS.DWWKJ3GCY4SX6SYXOFIHEA2SUMFRC6UQSE7BFFI.YAJWGXZJTYZAXKPE7NJBF6MDPMGH5CNYAZ4XHHBUIETCGFLA4EEA'
-# TODO: Hide API Key
+# Function to convert CSV to JSON
+def csv_to_json(filename):
+    data = []
+    with open(filename, mode='r', newline='') as file:
+        csv_reader = csv.DictReader(file)
+        for row in csv_reader:
+            data.append(row)
+    return data
 
-data_type = 'uplink_message'
-device_url = f"https://eu1.cloud.thethings.network/api/v3/as/applications/{application_id}/devices/{device_id}/packages/storage/{data_type}"
 
-headers = {
-    'Authorization': f'Bearer {access_key}',
-    'Accept': 'application/json'
-}
-with open('weather_data.csv', 'w', newline='') as file:
-    writer = csv.writer(file)
-    writer.writerow(['Measurement Value', 'Type', 'Received At'])
+# Endpoint to for getting data
+@app.route('/data', methods=['GET'])
+def get_data():
+    try:
+        data = csv_to_json('weather_data.csv')
+        return jsonify(data)
+    except FileNotFoundError:
+        return jsonify({'error': 'File not found'}), 404
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
 
 scheduler = BackgroundScheduler()
 scheduler.add_job(fetch_and_process_data, 'interval', minutes=15)  # Set interval here
 scheduler.start()
 
-try:
+
+#  Endpoint to fetch data immediately
+@app.route('/fetch', methods=['GET'])
+def start_fetching():
     fetch_and_process_data()
-    while True:
-        print("Running..." + " Time: " + time.strftime("%H:%M"))
-        time.sleep(70)
-except (KeyboardInterrupt, SystemExit):
-    scheduler.shutdown()
-    print("Scheduler shut down successfully!")
+    return jsonify({'status': 'fetched data successfully'})
+
+
+# Headers for API request
+headers = {
+    'Authorization': f'Bearer {config.access_key}',
+    'Accept': 'application/json'
+}
+
+# Initialize CSV
+with open('weather_data.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(['Measurement Value', 'Type', 'Received At'])
+
+if __name__ == '__main__':
+    try:
+        app.run(debug=True, use_reloader=False)
+    except (KeyboardInterrupt, SystemExit):
+        scheduler.shutdown()
+        print("Scheduler shut down successfully!")
