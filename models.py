18845e4fbf7e157db8bb2ad05ced6691087ce1c0
tux <ma.knigge@ostfalia.de>
2024-05-17 15:44:24 +0200

    Fixed scheduler.py to call the new function for fetching forecast and modified the days

--- models.py
+++ models.py
@@ -2,14 +2,12 @@ from app import db
 from datetime import datetime
 
 
-
 class WeatherStationData(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     measurement_value = db.Column(db.Float, nullable=False)
     measurement_type = db.Column(db.String(50), nullable=False)
     received_at = db.Column(db.String(120), nullable=False)
     fetched_at = db.Column(db.String(120), nullable=False)
-
 
     def __repr__(self):
         return f"<WeatherStationData {self.measurement_type} {self.received_at}>"
@@ -23,7 +21,6 @@ class WeatherForecastData(db.Model):
     sunshine_duration_minutes = db.Column(db.Integer)
     precipitation_mm = db.Column(db.Float)
     fetched_at = db.Column(db.String(120), nullable=False)
-
 
     def __repr__(self):
         return f"<WeatherForecastData {self.date}>"
