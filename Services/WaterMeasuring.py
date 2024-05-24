e13b0aae0af805096cb61976c02a38975f2c288e
tux <ma.knigge@ostfalia.de>
2024-05-24 15:22:35 +0200

    Changed volume calculation again #6

--- Services/WaterMeasuring.py
+++ Services/WaterMeasuring.py
@@ -38,7 +38,7 @@ def calculate_and_store_volume(db, WaterManagementData):
 
         new_data = WaterManagementData(
             date=datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
-            volume=volume.__round__(1) / 1000,
+            volume=round(volume, 1) / 1000 ,
             fetched_at=datetime.utcnow()
         )
         db.session.add(new_data)
