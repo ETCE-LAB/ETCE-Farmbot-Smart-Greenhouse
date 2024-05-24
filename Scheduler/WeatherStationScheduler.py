fa70de349f30909a45a5d88fd9ed71efc2e4a525
tux <ma.knigge@ostfalia.de>
2024-05-24 15:38:53 +0200

    Made code ready to run on Raspberry

--- Scheduler/WeatherStationScheduler.py
+++ Scheduler/WeatherStationScheduler.py
@@ -1,5 +1,6 @@
 from apscheduler.schedulers.background import BackgroundScheduler
 
+from Services.WaterMeasuring import calculate_and_store_volume
 from Services.WeatherPredictionService import fetch_weather_forecast_range
 from Services.WeatherStationService import fetch_and_process_data
 
@@ -9,4 +10,5 @@ scheduler = BackgroundScheduler()
 
 scheduler.add_job(fetch_and_process_data, 'interval', minutes=15)
 scheduler.add_job(fetch_weather_forecast_range, 'interval', hours=8, args=[(datetime.datetime.now()).strftime('%Y-%m-%d'), (datetime.datetime.now() + datetime.timedelta(days=15)).strftime('%Y-%m-%d')])
+scheduler.add_job(calculate_and_store_volume, 'interval', hours=24)
 # TODO: remove forecast from here and make it work in a separate scheduler
