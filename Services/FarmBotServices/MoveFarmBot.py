25a541dd3dfcdbc9464c91690e6ee2aa3c0b84ca
mrsoheilnezakat <contact@soheiln.tech>
2024-11-10 14:49:23 +0100

    new method as per new library applied, without handler. mush easier and simpler

--- Services/FarmBotServices/MoveFarmBot.py
+++ Services/FarmBotServices/MoveFarmBot.py
@@ -2,52 +2,15 @@ from urllib.error import HTTPError
 from farmbot import Farmbot
 import config
 
-
-class MyHandler:
-    def __init__(self, target_x, target_y, target_z):
-        self.target_x = target_x
-        self.target_y = target_y
-        self.target_z = target_z
-
-    def on_connect(self, bot, mqtt_client):
-        bot.move_absolute(x=self.target_x, y=self.target_y, z=self.target_z)
-        bot.send_message("Moving to target coordinates...")
-
-    def on_change(self, bot, state):
-        print("Current position: (%.2f, %.2f, %.2f)" % bot.position())
-        pos = state["location_data"]["position"]
-        xyz = (pos["x"], pos["y"], pos["z"])
-
-        if xyz == (self.target_x, self.target_y, self.target_z):
-            print(
-                f"FarmBot has reached the coordinates ({self.target_x}, {self.target_y}, {self.target_z}). Disconnecting from FarmBot.")
-            raise KeyboardInterrupt
-
-    def on_log(self, bot, log):
-        print("New message from FarmBot: " + log['message'])
-
-    def on_response(self, bot, response):
-        print("ID of successful request: " + response.id)
-        bot.send_message("Successfully moved to target coordinates")
-
-    def on_error(self, bot, response):
-        print("ID of failed request: " + response.id)
-        print("Reason(s) for failure: " + str(response.errors))
-        bot.send_message("Failed to move to target coordinates")
-
-
 def move_to(x, y, z):
     email = config.farmbot_email
     password = config.farmbot_password
     url = config.farmbot_url
-
     fb = Farmbot()
     token=fb.get_token(email,password,url)
     fb.set_token(token)
-
-    handler = MyHandler(x, y, -z)
     try:
-        fb.connect(handler)
+        fb.move(x=x,y=y,z=-z)
     except KeyboardInterrupt:
         return {
             'status': 'FarmBot has reached the target coordinates',
