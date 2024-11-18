b8c6291a6bbddbbe87994c70ac9235d54b8f71d4
mrsoheilnezakat <contact@soheiln.tech>
2024-11-18 19:48:06 +0100

    Humidity temperature scheduler added

--- Scheduler/GreenHouseScheduler.py
+++ Scheduler/GreenHouseScheduler.py
@@ -5,4 +5,6 @@ Green_house_service=GreenHouseService()
 
 scheduler_service = SchedulerService()
 
-scheduler_service.add_job(Green_house_service.measure_soil_moisture, 'interval', minutes=30)  # run every hour
+scheduler_service.add_job(Green_house_service.measure_temp_humidity, 'interval', minutes=1)  # run every hour
+
+scheduler_service.add_job(Green_house_service.measure_soil_moisture, 'interval', hours=8)  # run every hour
